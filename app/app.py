794c3ab8e69ca96d6c3584bbcf8497e2082bfba0
Khasir <khasir.hean@gmail.com>
2023-09-06 14:51:41 -0400

    add /gif endpoint and specify additional arguments to yt-dlp

--- app/app.py
+++ app/app.py
@@ -1,3 +1,7 @@
+"""
+Based on: https://github.com/GoogleCloudPlatform/cloud-run-microservice-template-python/blob/main/app.py
+"""
+
 import os
 import signal
 import sys
@@ -5,6 +9,7 @@ from types import FrameType
 
 from flask import Flask, request, send_file
 from yt_dlp import YoutubeDL
+# from yt_dlp.postprocessor import EmbedThumbnailPP
 
 
 app = Flask(__name__)
@@ -18,37 +23,53 @@ def authenticate():
     if AUTHENTICATION_PASSWORD and password != AUTHENTICATION_PASSWORD:
         return "Unauthorized", 401
 
-
 @app.get("/")
 def main() -> tuple:
+    return "Hello, world!", 200
+
+
+@app.get("/video")
+def get_video() -> tuple:
     # print(request.args)
     video_id = request.args.get("id")
     if not video_id:
         return "Please specify `id`", 400
-    return download(video_id)
+    return download_video(video_id)
 
-    # return "Hello, world!", 200
+
+@app.get("/gif")
+def get_gif() -> tuple:
+    video_id = request.args.get("id")
+    if not video_id:
+        return "Please specify `id`", 400
+    return download_gif(video_id)
 
 
 @app.get("/test")
 def test():
     # Test video
-    return download('BaW_jenozKc')
+    return download_video('BaW_jenozKc')
 
 @app.get("/robots.txt")
 def robots():
     """
     Disallow all robots.
     """
-    return """User-agent: *
-Disallow: /""", 200
+    return "User-agent: *\nDisallow: /", 200
 
 
-def download(video_id: str):
+def download_video(video_id: str):
     # Download video
-    URLS = [f'https://www.youtube.com/watch?v={video_id}']
-    with YoutubeDL() as ydl:
-        ydl.download(URLS)
+    # The request below is equivalent to:
+    #   yt-dlp -f "bestvideo*[ext=mp4][filesize<=20M]+bestaudio[ext=m4a][filesize<=4M] / bestvideo*[filesize<=19M]+bestaudio[filesize<=4M] / best[filesize<=25M] / best" "https://www.youtube.com/watch?v={video_id}"
+    # which will find the best video and audio combination that is preferably less than 25MB (with multiple fallbacks)
+    urls = [f'https://www.youtube.com/watch?v={video_id}']
+    parameters = {
+        # "postprocessors": [{"key": "EmbedThumbnail"}],
+        "format": "bestvideo*[ext=mp4][filesize<=20M]+bestaudio[ext=m4a][filesize<=4M] / bestvideo*[filesize<=19M]+bestaudio[filesize<=4M] / best[filesize<=25M] / best"
+    }
+    with YoutubeDL(params=parameters) as ydl:
+        ydl.download(urls)
 
     # Locate downloaded file
     filenames = os.listdir(".")
@@ -56,9 +77,43 @@ def download(video_id: str):
     if not video:
         raise FileNotFoundError("An error has occurred")
     video = video[0]
-
     print("video:", video)
+
+    # TODO overcome Cloud Run 32MB limit: https://cloud.google.com/run/quotas
+    # as_attachment indicates to the browser to download instead of displaying
     return send_file(video, as_attachment=True)
+
+
+def download_gif(video_id: str):
+    # Download video
+    # The request below is equivalent to:
+    #   yt-dlp -f "bv[filesize<=512K] / wv" --ppa "VideoConvertor:-r 8" --recode gif "https://www.youtube.com/watch?v={video_id}"
+    # which will find the best video (no audio) under 512KB, or the worst video if that isn't available, and recode to gif at 8 fps, ignoring audio
+    urls = [f'https://www.youtube.com/watch?v={video_id}']
+    parameters = {
+        "format": "bv[filesize<=512K] / wv",
+        "postprocessors": [{
+            "key": "FFmpegVideoConvertor",
+            "preferedformat": "gif"
+        }],
+        "postprocessor_args": {
+            "VideoConvertor": "-r 8 -an",
+        },
+    }
+    with YoutubeDL(params=parameters) as ydl:
+        ydl.download(urls)
+
+    # Locate downloaded file
+    filenames = os.listdir(".")
+    gif = [filename for filename in filenames if filename.endswith(".gif")]
+    if not gif:
+        raise FileNotFoundError("An error has occurred")
+    gif = gif[0]
+    print("video:", gif)
+
+    # TODO overcome Cloud Run 32MB limit: https://cloud.google.com/run/quotas
+    # as_attachment indicates to the browser to download instead of displaying
+    return send_file(gif, as_attachment=True)
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
