f6f2e73ce2983bd54f77ef2c85dd8fb9d9899feb
Khasir <khasir.hean@gmail.com>
2023-09-05 23:28:40 -0400

    video download and transfer is working

--- app/app.py
+++ app/app.py
@@ -1,16 +1,38 @@
+import os
 import signal
+import subprocess
 import sys
 from types import FrameType
 
-from flask import Flask
+from flask import Flask, send_file
+from yt_dlp import YoutubeDL
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.get("/")
 def main() -> tuple:
     return "Hello, world!", 200
+
+
+@app.get("/test")
+def test():
+    # Download video
+    URLS = ['https://www.youtube.com/watch?v=BaW_jenozKc']
+    with YoutubeDL() as ydl:
+        ydl.download(URLS)
+    # subprocess.run(["./youtube-dl", "https://www.youtube.com/watch?v=Rq83QosbjZ0"])
+
+    # Locate downloaded file
+    filenames = os.listdir(".")
+    video = [filename for filename in filenames if filename.endswith(".mp4")]
+    if not video:
+        raise FileNotFoundError("An error has occurred")
+    video = video[0]
+
+    print("video:", video)
+    return send_file(video)
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
@@ -23,7 +45,7 @@ def shutdown_handler(signal_int: int, frame: FrameType) -> None:
 if __name__ == "__main__":
     # Running application locally, outside of a Google Cloud Environment
 
-    # handles Ctrl-C termination
+    # Handles Ctrl-C termination
     signal.signal(signal.SIGINT, shutdown_handler)
 
     app.run(host="localhost", port=8080, debug=True)
